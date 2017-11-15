33f1785ace8d2d6734c8b245df032150a05c10d9
erratbi <yerratbi@gmail.com>
2017-11-15 18:53:21 +0000

    Searching books part 1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import Promise from 'bluebird';
 import auth from './routes/auth';
 import users from './routes/users';
+import books from './routes/books';
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
+app.use('/api/books', books);
 
 app.get('/*', (req, res) => {
 	res.send('This is an api service');
---
+++ src/routes/books.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/search/:query', (req, res) => res.json({
+		books: [
+			{
+				goodreadId: 1,
+				title: '1986',
+				authors: 'George Orwell',
+				covers: [
+					'https://images.gr-assets.com/books/1348990566l/5470.jpg',
+					'https://images.gr-assets.com/books/1504611957l/9577857.jpg',
+				],
+				pages: 198,
+			},
+			{
+				goodreadId: 2,
+				title: 'Three Men in a Boat',
+				authors: 'Jerome K. Jerome',
+				covers: [
+					'https://images.gr-assets.com/books/1392791656l/4921.jpg',
+					'https://images.gr-assets.com/books/1312036878l/627830.jpg',
+				],
+				pages: 256,
+			},
+		],
+	}));
+
+export default router;
